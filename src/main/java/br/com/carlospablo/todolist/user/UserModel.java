b6dfd83373e694c4c6bf49ba7e30e577763352b9
Carlos Pablo <carlospablo796@gmail.com>
2025-11-21 18:18:19 -0300

    Commit Inicial

---
+++ src/main/java/br/com/carlospablo/todolist/user/UserModel.java
@@ -0,0 +1,14 @@
+package br.com.carlospablo.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String name;
+    private String username;
+    private String password;
+
+   
+
+
+}
