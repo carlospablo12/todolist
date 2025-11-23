b8e873b0ffc9dd6b2932ed7f03ddf2c0a879621d
Carlos Pablo <carlospablo796@gmail.com>
2025-11-23 15:26:59 -0300

    Criando tabela Usuario e Repository

--- src/main/java/br/com/carlospablo/todolist/user/UserController.java
+++ src/main/java/br/com/carlospablo/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.carlospablo.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel){
+       var userCreated =  this.userRepository.save(userModel);
+       return userCreated;
     }
 
     
