26c336f5a6637063d857bca23112f597820e8344
Carlos Pablo <carlospablo796@gmail.com>
2026-02-04 21:42:48 -0300

    Implementa Lista Usuário

--- src/main/java/br/com/carlospablo/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/carlospablo/todolist/task/ITaskRepository.java
@@ -3,7 +3,10 @@ package br.com.carlospablo.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    TaskModel findByDescription(String description);
+    // TaskModel findByDescription(String description);
+    List<TaskModel> findByIdUser(UUID idUser);
 }
