5d1f2fb87e51b255fe18e50e041857552f421d9b
Carlos Pablo <carlospablo796@gmail.com>
2026-02-10 18:18:06 -0300

    Implementa update(put) na task

--- src/main/java/br/com/carlospablo/todolist/task/TaskController.java
+++ src/main/java/br/com/carlospablo/todolist/task/TaskController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.carlospablo.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -54,4 +57,18 @@ public class TaskController {
         var tasks = this.iTaskRepository.findByIdUser((UUID)idUser);
         return tasks;
     }
+    // Update
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id ){
+       
+
+        var task = this.iTaskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        
+        return this.iTaskRepository.save(task);
+        
+    }
+
 }
